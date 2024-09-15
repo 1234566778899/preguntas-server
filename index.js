7f7d35b7a6d21646c73b48d255daef957380f53e
Carlos Jesús Ordaz Hoyos <carlos.ordaz@acomo.com.pe>
2024-09-15 01:41:10 -0500

    xx

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const port = process.env.PORT || 4000;
 let salas = new Map();
 io.on('connection', (socket) => {
     socket.on('enviar-nombre', (data) => {
-        console.log('name: ' + socket.id);
         socket.join(data.codigo);
         socket.sala = data.codigo;
         if (salas.has(data.codigo)) {
